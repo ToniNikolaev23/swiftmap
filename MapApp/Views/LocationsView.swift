977e2dce6e23580a6dcc8268c92c4759fe07d2e8
Toni Stoyanov <toni.n.stoyanov@icloud.com>
2024-12-12 09:28:01 +0200

    Add assets, add Location Model and Fake data

---
+++ MapApp/Views/LocationsView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationsView.swift
+//  MapApp
+//
+//  Created by Toni Stoyanov on 12.12.24.
+//
+
+import SwiftUI
+
+struct LocationsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationsView()
+}
